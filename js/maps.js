47501fc93e5cd6305c600c34d3162109f155c55d
YouFive <84190472+TheYouFive@users.noreply.github.com>
2022-07-27 22:04:25 +0900

    marker

--- js/maps.js
+++ js/maps.js
@@ -1,11 +1,11 @@
-let mapOptions = {
+var mapOptions = {
   center: new naver.maps.LatLng(37.85286679999975, 127.19849449999991),
   zoom: 10,
 };
 
-let marker = new naver.maps.Marker({
+var marker = new naver.maps.Marker({
   position: new naver.maps.LatLng(37.85286679999975, 127.19849449999991),
   map: map,
 });
 
-let map = new naver.maps.Map("map", mapOptions);
+var map = new naver.maps.Map("map", mapOptions);
